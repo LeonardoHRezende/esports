4313f2bf5958aef57da532de33bea4b9cf6b5f2c
leonardo-assertech <leonardo-rezende@assertech.app>
2022-09-13 00:33:42 -0300

    Style: estilização de elementos em tela

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,7 @@
 import './styles/main.css'
 
+import { MagnifyingGlassPlus } from 'phosphor-react'
+
 import Logo from './assets/logo-nlw-esports.svg'
 
 function App() {
@@ -64,8 +66,16 @@ function App() {
 
         </div>
 
-        <div className="bg-[#2a2634] px-8 py-6 mt-8 self-stretch rounded-lg">
-
+        <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden mt-8 ">
+          <div className="bg-[#2a2634] px-8 py-6 before: w-full flex justify-between">
+            <div>
+              <strong className="text-2xl text-white font-black block">Não encontrou seu duo?</strong>
+              <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
+            </div>
+            <button className="py-3 px-4 bg-violet-500 text-white rounded hover:bg-violet-600 flex items-center gap-3">
+             <MagnifyingGlassPlus size={24}/> Publicar Anúncio
+            </button>
+          </div>
         </div>
 
       </div>
