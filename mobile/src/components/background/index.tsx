80737d75e36bc592368b9d5149e03d2934067c02
Leonardo Henrique Rezende <61363823+LeonardoHRezende@users.noreply.github.com>
2022-09-13 20:05:55 -0300

    Feat: Preparação do ambiente mobile

---
+++ mobile/src/components/background/index.tsx
@@ -0,0 +1,23 @@
+import { ImageBackground } from 'react-native';
+
+import BackgroundImg from '../../assets/background-galaxy.png'
+
+import { styles } from './styles';
+
+interface Props {
+    children: React.ReactNode;
+}
+
+export default function Background({ children }: Props) {
+    return (
+        <ImageBackground
+            source={BackgroundImg}
+            style={styles.container}
+            defaultSource={BackgroundImg}
+        >
+
+            {children}
+
+        </ImageBackground>
+    );
+}
