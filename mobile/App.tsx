a8247645ddea7cb8e1c03bf1bae90e5df10d58db
leonardo-assertech <leonardo-rezende@assertech.app>
2022-09-13 23:37:41 -0300

    Feat: Criação de components

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,19 +1,20 @@
 import { StatusBar } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from '@expo-google-fonts/inter'
 
-import Background from './src/components/background';
-
+import Background from './src/components/Background';
+import { Home } from './src/screens/home';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
 
   const [fontsLoaded] = useFonts({
-    Inter_400Regular, 
-    Inter_600SemiBold, 
-    Inter_700Bold, 
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
     Inter_900Black
 
   });
-  
+
   return (
     <Background>
 
@@ -21,8 +22,9 @@ export default function App() {
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
-      />
 
+      />
+      {fontsLoaded ? <Home /> : <Loading />}
     </Background>
   );
 }
