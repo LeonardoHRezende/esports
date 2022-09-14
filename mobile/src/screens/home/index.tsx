de0f20c16806c7f94aec010d54c2dcd55b558d74
Leonardo Henrique Rezende <61363823+LeonardoHRezende@users.noreply.github.com>
2022-09-14 13:59:32 -0300

    Feat: Adição de cards

--- mobile/src/screens/home/index.tsx
+++ mobile/src/screens/home/index.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import { View } from 'react-native';
+import { View, Image } from 'react-native';
+
+import LogoImg from '../../assets/logo-nlw-esports.png';
+import { Gamecard } from '../../components/Gamecard';
+import { Heading } from '../../components/Heading';
+import { GAMES } from '../../utils/games';
 
 import { styles } from './styles';
 
 export function Home() {
     return (
         <View style={styles.container}>
+            <Image
+                source={LogoImg}
+                style={styles.logo}
+            />
+
+            <Heading
+                title="Encontre seu duo!"
+                subtitle="Selecione o game que deseja jogar..."
+            />
+
+            <Gamecard
+                data={GAMES[0]}
+            />
 
         </View>
     );
