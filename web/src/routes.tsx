7c4424c5126f2a1f7987e45b15a7751246506045
Leonardo Henrique Rezende <61363823+LeonardoHRezende@users.noreply.github.com>
2022-10-10 14:25:18 -0300

    Feat: Adição de rota privada verificando se tem o identificador para poder acessar

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,15 +1,31 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate, Outlet } from 'react-router-dom';
 
 import Home from "../src/pages/index";
 import Login from "../src/pages/login";
 
 export default function Rotas() {
+
+    //VERIFICANDO SE TEM O ID PARA CONTINUAR
+    const PrivateRoute = () => (
+
+        sessionStorage.getItem('identify') ? (
+            <Outlet />
+        ) : (
+            <Navigate to="/login" />
+        )
+    );
+
     return (
         <BrowserRouter>
 
             <Routes>
-                <Route path="/" element={<Home />} />
+
+                <Route path="/" element={<PrivateRoute />}>
+                    <Route path="/" element={<Home />} />
+                </Route>
+
                 <Route path="/login" element={<Login />} />
+
             </Routes>
 
         </BrowserRouter>
